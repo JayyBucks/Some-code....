1aebafcced59b9c11ce7889105351f0737bbd8f7
JayyBucks <williamsgerard515@gmail.com>
2025-06-09 11:29:48 -0500

    adding my first python program

---
+++ first.py
@@ -0,0 +1,46 @@
+# print("Hello World!")
+# #Hi guys!
+# #Create a variable to store my name
+first_name = "Gerard"
+
+# #print "My name is Gerard"
+
+# print("My name is", first_name + "!")
+
+# #create a variable to store my last name
+
+last_name = "Williams"
+
+# #write a statement to display "My full name is Gerard Williams!"
+# print("My full name is", first_name, last_name + "!")
+
+# #variables to store your age, height, weight
+
+age = 16
+weight = 111
+height = 65
+
+# #print a sentence with age, weight, height,
+
+# print(f"My name is {first_name} {last_name}!\nI am {age} years old and I weigh {weight}lbs.\nI am {height} inches tall!")
+
+
+# #get and print the data type for age, weight, and height
+
+# print(type(age))
+# print(type(weight))
+# print(type(height))
+
+#write 3 print statements using string interpolation (fstring) to print descriptive sentences for the data types: Variable age is an int
+
+# print(f"Variable {age} is {type(age)}\nVariable {height} is {type(height)}\nVariable {weight} is {type(weight)}, too!")
+
+num_1 = 2
+num_2 = 8
+total= num_1 + num_2
+print(f"Total: {total}")
+
+num_1 = "2"
+num_2 = "8"
+total= num_1 + num_2
+# print(f"Total: {total}")

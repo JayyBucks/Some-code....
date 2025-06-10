66ecfa02d07f75b8fc752ad3309cd22984bff70e
JayyBucks <williamsgerard515@gmail.com>
2025-06-10 11:25:49 -0500

    added two files

---
+++ code_two.py
@@ -0,0 +1,27 @@
+def lbs_to_kgs():
+    while True:
+        try:
+            weight = float(input("How much do you weigh in pounds?\n"))
+            if weight > 0:
+                kilograms = weight/2.205
+                print(f"You weigh {kilograms:.2f} kilograms!")
+                break
+            else:
+                print("ERROR: Please enter a positive number!")
+        except:
+            print("ERROR: Please only enter a number!")
+    return weight
+def positive_float_value():
+    while True:
+        try:
+            num = float(input("Enter a number: \n"))
+            if num > 0:
+                break
+            else:
+                print("ERROR: Please enter a positive number!")
+        except:
+            print("ERROR: Please only enter a number!")
+    return num
+def main():
+    lbs_to_kgs()
+    positive_float_value()
---
+++ code_twopointfive.py
@@ -0,0 +1,12 @@
+out_value = 0
+stop_value = 10
+
+while out_value <= stop_value:
+    print(out_value)
+    out_value = out_value + 1
+
+print("\n\n")
+output_value = 0
+while True:
+    print(out_value)
+    out_value += 1

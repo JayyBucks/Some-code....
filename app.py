4c1329cc86f9af9f569f23bae473eb9ed6af7152
JayyBucks <williamsgerard515@gmail.com>
2025-06-27 10:43:10 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,48 @@
 from flask import Flask, render_template, request, url_for, redirect, abort, flash
 import requests
 
-#make a Flask app
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 app.config["SECRET_KEY"] = "your secret key"
 
-"""
-Function to request student data from the api
-Input: url
-Output: JSON student data
-"""
 def get_student_data(url:str):
-    #make a request
     response = requests.get(url)
 
-    #convert the response format to JSON
-    response_json = response.json
+    response_json = response.json()
     return response_json
-
-#create a route for the website index/root. Will display all student data
 @app.route('/', methods=['GET'])
 def index():
-    #make a request to teh student data api
-    url = ' http://127.0.0.1:5000/api/students/all'
+    url = 'http://127.0.0.1:5000/api/students/all'
 
     student_data = get_student_data(url)
-    return render_template('index.html')
+    return render_template('index.html', student_data=student_data)
+@app.route('/majors', methods=['GET'])
+def majors_get():
+    url = 'http://127.0.0.1:5000/api/students/all'
+    student_data = get_student_data(url)
+    major_list = []
+    for student in student_data:
+        if student['major'] not in major_list:
+            major_list.append(student['major'])
+
+        major_list.sort()
+    return render_template('majors.html', major_list=major_list)
+
+@app.route('/majors', methods=['POST'])
+def majors_post():
+    url = 'http://127.0.0.1:5000/api/students/all'
+    student_data = get_student_data(url)
+    major_list = []
+    for student in student_data:
+        if student['major'] not in major_list:
+            major_list.append(student['major'])
+
+    major_list.sort()
+    major = request.form.get('major')
+    print(major)
+    url =f"http://127.0.0.1:5000/api/majors/{major}"
+    result_list = get_student_data(url)
+    return render_template('majors.html', major_list=major_list, result_list=result_list)
 
 app.run(port=5001)
